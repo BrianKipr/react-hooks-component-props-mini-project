77bb01f95826ecc3d0b1829531e52ab82ba070c8
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-07-20 20:39:18 +0300

    assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
+// import React from "react";
+// import blogData from "../data/blog";
+
+// console.log(blogData);
+
+// function App() {
+//   return (
+//     <div className="App">
+//       You're on your own from here! Follow the deliverables; test things out in
+//       the browser as you write your code; and good luck!
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
 import blogData from "../data/blog";
-
-console.log(blogData);
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header blogName="My Blog" />
+      <About image="https://petapixel.com/assets/uploads/2022/12/what-is-unsplash-800x420.jpg" aboutText="About my blog..." />
+      <ArticleList articles={blogData.articles} />
     </div>
   );
 }
